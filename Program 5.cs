23d775a68c9f69b2cbd937f7738eabcb07c591b9
DwanilSanchez30 <78990873+DwanilSanchez30@users.noreply.github.com>
2021-03-04 21:47:46 -0400

    Add files via upload
    Dwanil R. Sanchez
    Matricula: 2020-10553

---
+++ Program 5.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Array_5
+{
+    class Program
+    {
+        /*5. Un programa que prepare espacio para un máximo de 100 nombres.El usuario deberá ir introduciendo un nombre cada vez, 
+        hasta que se pulse Intro sin teclear nada, momento en el que dejarán de pedirse más nombres y se mostrará en 
+        pantalla la lista de los nombres que se han introducido.*/
+
+        static void main(string[] args)
+        {
+            string[] Nombres = new string[100];
+            int i = 0;
+            do
+            {
+                Console.WriteLine("Ingrese un nombre");
+                Nombres[i] = Console.ReadLine();
+
+            } while (Nombres[i] != "");
+            Console.WriteLine();
+
+            for (i = 0; i = 100; i++) ;
+            Console.WriteLine("los nombres tecleados son los de arriba {0}", Nombres[i]);
+        } 
+    }
+}
+        
---
+++ Program 8.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Array_8
+{
+    class Program
+    {
+    /*8. Crear un programa que defina un array de 5 elementos de tipo float que representen las alturas de 5 personas.
+    Obtener el promedio de las mismas. Contar cuántas personas son más altas que el promedio y cuántas más bajas.*/
+        private float[] Alturas;
+        private float Promedio;
+        public void Agregar()
+        {
+            Alturas = new float[5];
+        
+            for (int f = 0; f < 5; f++)
+            {
+                Console.Write("Ingrese altura de una persona: ");
+                string linea = Console.ReadLine();
+                Alturas[f] = float.Parse(linea);
+            }
+        }
+        public void CPromedio()
+        {
+            float suma;
+            suma = 0;
+            for (int f = 0; f < 5; f++)
+            {
+                suma = suma + Alturas[f];
+            }
+            Promedio = suma / 5;
+            Console.WriteLine("Promedio de las alturas es: " + Promedio);
+        }
+        public void DiferenciasAlturas()
+        {
+            int Mayor, Menor;
+
+            Mayor = 0;
+            Menor = 0;
+            for (int f = 0; f < 5; f++)
+            {
+                if (Alturas[f] > Promedio)
+                {
+                    Mayor++;
+                }
+                else
+                {
+                    if (Alturas[f] < Promedio)
+                    {
+                        Menor++;
+                    }
+                }
+            }
+            Console.WriteLine("Cantidad de personas Mayores al Promedio: " + Mayor);
+            Console.WriteLine("Cantidad de personas Menores al Promedio: " + Menor);
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            Program Alt = new Program();
+            Alt.Agregar();
+            Alt.CPromedio();
+            Alt.DiferenciasAlturas();
+        }
+    }
+}
